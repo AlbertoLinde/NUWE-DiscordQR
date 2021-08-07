3622c0fe1508d69b5269d9e588e8acf497848354
Alberto <abreulindealberto@gmail.com>
2021-08-07 14:57:11 +0100

    Added color setting when generate the QR, don't use anymore the Google API

--- src/commands/urltoqr.js
+++ src/commands/urltoqr.js
@@ -1,35 +1,60 @@
-const utils = require("../utils/utils");
+const Discord = require('discord.js');
 const { MessageEmbed } = require('discord.js');
 
+const QRCode = require('qrcode')
+const utils = require("../utils/utils");
+
+/**
+ * Filter to avoid the use of switch.
+ */
+const COLORS = {
+	RED: '#F13434',
+	BLUE: '#08BBD4',
+	ORANGE: '#EEB913',
+	GREEN: '#8ADF65',
+	BLACK: '#000000',
+};
 
 module.exports = {
 	name: 'urltoqr',
 	description: 'Convert a URL to QR',
 	execute(message, args) {
 		if (utils.isUrl(args[0])) {
-			this.sendEmbed(message, this.generateEmbed(message, args));
-		}else{
+			this.generateQRCode(message, args)
+		} else if (utils.isUrl(args).length < 1) {
 			message.channel.send(`❗ ERROR ${message.author} check the URL.`)
 		}
-
 	},
+	generateQRCode(message, args) {
 
-	generateEmbed(message, args) {
-		const embed = new MessageEmbed()
-			.setColor(0x318735)
-			.setTitle("Nuwe QR Generator")
-			.setImage(`https://chart.googleapis.com/chart?cht=qr&chs=250x250&chl=${args[0]}`)
-			.setDescription(`${message.author} your QR code has been generated.`)
-			.setFooter("BOT Created by Alberto Linde", "https://avatars.githubusercontent.com/u/44638858?s=400&u=b1b1027ca6baa155842a21beeefb8a800f85e7ec&v=4")
-			.setURL("https://github.com/AlbertoLinde/NUWE-DiscordQR");
-		return embed;
-	},
+		let colorSelected = COLORS[args[1]] || COLORS.BLACK;
 
-	sendEmbed(message, embed) {
-		message.author.send(embed);
-		message.channel.send(embed);
+		QRCode.toDataURL(args[0], {
+			color: {
+				dark: colorSelected,
+				light: '#FFFFFF'
+			}
+		}, function generateEmbed(err, url) {
+			if (err) {
+				message.channel.send("💥 Can't generate your QR. Please, try again.");
+			}
+
+			/** Get the QR and don't save the file, stay on the buffer and use as attachment in the embed message */
+			const buffer = new Buffer.from(url.split(',')[1], 'base64');
+			const file = new Discord.MessageAttachment(buffer, 'img.png');
+
+			const embed = new MessageEmbed()
+				.setColor(COLORS.GREEN)
+				.setTitle("Nuwe QR Generator")
+				.attachFiles(file)
+				.setImage('attachment://img.png')
+				.setDescription(`${message.author} your QR code has been generated.`)
+				.setFooter("BOT Created by Alberto Linde", "https://avatars.githubusercontent.com/u/44638858?s=400&u=b1b1027ca6baa155842a21beeefb8a800f85e7ec&v=4")
+				.setURL("https://github.com/AlbertoLinde/NUWE-DiscordQR");
+
+			/** Send the message to the channel on Discord and send DM to the user */
+			message.author.send(embed);
+			message.channel.send(embed);
+		});
 	}
-
 };
-
-
